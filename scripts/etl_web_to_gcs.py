bcfb74d321e6ce9bca0c4c582614d5dd5843f5bf
Southboy12 <menkachi85@gmail.com>
2023-03-26 15:35:35 +0100

    Added  transform and etl_to_gcs functions

--- scripts/etl_web_to_gcs.py
+++ scripts/etl_web_to_gcs.py
@@ -4,20 +4,35 @@ from pathlib import Path
 import os
 
 
-def extract_to_local():
+def extract_to_local(url: str) -> pd.DataFrame:
+    """Download the dataset from kaggle and put in a DataFrame"""
     url = 'https://www.kaggle.com/datasets/garrickhague/world-earthquake-data-from-1906-2022/download?datasetVersionNumber=2'
     path = Path(f"./data")
     od.download(url, path)
-    file_path = f"data/world-earthquake-data-from-1906-2022/Global_Earthquake_Data.csv"
-    df = pd.read_csv(file_path)
-    df.to_parquet("./data/world-earthquake-data-from-1906-2022/Global_Earthquake_Data.parquet", compression="gzip")
-    print(df.info())
+    csv_file_path = f"data/world-earthquake-data-from-1906-2022/Global_Earthquake_Data.csv"
+    df = pd.read_csv(csv_file_path)
+    parquet_file_path = f"./data/world-earthquake-data-from-1906-2022/Global_Earthquake_Data.parquet"
+    df.to_parquet(parquet_file_path, compression="gzip")
+    return df
+
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df["time"] = pd.to_datetime(df['time'])
+    df["updated"] = pd.to_datetime(df["updated"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+def write_to_gcs()
    
 
 
 
-
-
+def etl_web_to_gcs():
+    url = "https://www.kaggle.com/datasets/garrickhague/world-earthquake-data-from-1906-2022/download?datasetVersionNumber=2"
+    df = extract_to_local(url)
+    transform_data(df)
 
 
 
@@ -26,4 +41,4 @@ def extract_to_local():
 
 
 if __name__ == "__main__":
-    extract_to_local()
+    etl_web_to_gcs()
